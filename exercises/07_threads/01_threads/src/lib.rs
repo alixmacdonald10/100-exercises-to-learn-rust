1c420b2f65ac272a83471a0506173c167579ce70
amackerel <amackerel@proton.me>
2026-08-28 09:02:09 +0100

    threads

--- exercises/07_threads/01_threads/src/lib.rs
+++ exercises/07_threads/01_threads/src/lib.rs
@@ -12,10 +12,28 @@
 // slices of the vector directly. You'll need to allocate new
 // vectors for each half of the original vector. We'll see why
 // this is necessary in the next exercise.
-use std::thread;
+use std::thread::{self, JoinHandle};
 
 pub fn sum(v: Vec<i32>) -> i32 {
-    todo!()
+
+    let mid = v.len() / 2;
+    let (lower, upper) = v.split_at(mid);
+    let lower = lower.to_vec();
+    let upper = upper.to_vec();
+
+    // split vector in half 
+    let handle_lower: JoinHandle<i32> = thread::spawn(move || {
+        lower.iter().sum() 
+    });
+
+    let handle_upper: JoinHandle<i32> = thread::spawn(move || {
+        upper.iter().sum() 
+    });
+
+    let lower_sum: i32 = handle_lower.join().unwrap();
+    let upper_sum: i32 = handle_upper.join().unwrap();
+
+    lower_sum + upper_sum
 }
 
 #[cfg(test)]
